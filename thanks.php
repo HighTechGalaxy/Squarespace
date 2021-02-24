0017725dac44d882ffb2e67cb3ce331ff5b6c92c
Technical-See <amitaec.786@gmail.com>
2021-02-24 16:08:26 +0530

    Redirect to a Thank You Page After Purchase

---
+++ thanks.php
@@ -0,0 +1,14 @@
+
+<head>
+The following code is used in the video.
+Redirect After a Given Amount of Time
+
+<meta http-equiv="refresh" content="10;URL='/thank-you'" />
+
+Redirect Immediately
+
+<meta http-equiv="refresh" content="0; url='/thank-you'"></meta>
+<style>#_confirmationpage {position:fixed; top:0; left:0; width:100%; height:100%; background:#fff; z-index:2147483646;}</style>
+<div id="_confirmationpage"></div>
+
+</head>
